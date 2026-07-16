f125e289eb2f8a8542e3b58c566eed802b90058a
Elton Aquino <87995437+elton-aq@users.noreply.github.com>
2026-07-15 23:28:21 -0300

    Solucao Lab02

--- src/drivers/serial.c
+++ src/drivers/serial.c
@@ -1,44 +1,108 @@
 #include <kernel/serial.h>
-#include <kernel/panic.h>
+#include <kernel/mm.h>
+#include <arch/io.h>
+#include <arch/csr.h>
+#include <arch/plic.h>
+#include <arch/spinlock.h>
+
+#define RX_BUF_SIZE 256
+
+static u8 *uart_base;
+
+static struct {
+	char rxbuf[RX_BUF_SIZE];
+	size_t rxlen;
+	struct spinlock lock;
+} uart;
+
+static inline void uart_write(u64 reg, u8 val)
+{
+	iowrite8(val, uart_base + reg);
+}
+
+static inline u8 uart_read(u64 reg)
+{
+	return ioread8(uart_base + reg);
+}
 
 void serial_init()
 {
-	/* not implemented */
-	BUG();
+	uart_base = (u8 *)pa_to_va((phys_addr_t)(u64)SERIAL_BASE);
+	uart.rxlen = 0;
+	spin_init(&uart.lock);
+
+	uart_write(SERIAL_IER, 0x00);
+	/* liga o DLAB pra poder configurar o divisor da taxa de transmissão */
+	uart_write(SERIAL_LCR, 0x80);
+	uart_write(SERIAL_RBR, 0x03);
+	uart_write(SERIAL_IER, 0x00);
+	/* desliga o DLAB e configura 8 bits, sem paridade, 1 stop bit */
+	uart_write(SERIAL_LCR, 0x03);
+	/* habilita as FIFOs de recepção/transmissão e limpa ambas */
+	uart_write(SERIAL_FCR, SERIAL_FCR_FIFO_ENABLE
+			 | SERIAL_FCR_RX_FIFO_CLEAR
+			 | SERIAL_FCR_TX_FIFO_CLEAR);
+	uart_write(SERIAL_MCR, 0x00);
 }
 
 void serial_irq_enable()
 {
-	/* not implemented */
-	BUG();
+	/* avisa a UART pra gerar interrupção */
+	uart_write(SERIAL_IER, SERIAL_IER_ERBFI);
+
+	/* configura o PLIC */
+	plic_irq_set_priority((u32)IRQ_SERIAL, 1);
+	plic_hart_enable_irq(0, (u32)IRQ_SERIAL);
+	plic_hart_set_threshold(0, 0);
+
+	/* habilita interrupções externas no sie */
+	csr_set(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	uart_write(SERIAL_IER, 0x00);
+	csr_clear(CSR_SIE, CSR_SIE_SEIE);
 }
 
 void serial_irq()
 {
-	/* not implemented */
-	BUG();
+	/* protege o buffer compartilhado com o serial_read() */
+	u64 flags = spin_lock_irqsave(&uart.lock);
+	while (uart_read(SERIAL_LSR) & SERIAL_LSR_DTR) {
+		u8 byte = uart_read(SERIAL_RBR);
+		if (uart.rxlen < RX_BUF_SIZE) {
+			uart.rxbuf[uart.rxlen++] = (char)byte;
+		}
+	}
+	spin_unlock_irqrestore(&uart.lock, flags);
 }
 
 size_t serial_read(char *buf)
 {
-	/* not implemented */
-	BUG();
-}
-
-void serial_puts(char *str)
-{
-	/* not implemented */
-	BUG();
+	/* copia tudo que tiver no buffer interno e zera ele*/
+	u64 flags = spin_lock_irqsave(&uart.lock);
+	size_t count = uart.rxlen;
+	for (size_t i = 0; i < count; i++) {
+		buf[i] = uart.rxbuf[i];
+	}
+	uart.rxlen = 0;
+	spin_unlock_irqrestore(&uart.lock, flags);
+	return count;
 }
 
 void serial_putc(char c)
 {
-	/* not implemented */
-	BUG();
+	/* espera o registrador de transmissão ficar livre antes de escrever */
+	while (!(uart_read(SERIAL_LSR) & SERIAL_LSR_THRE))
+		;
+	uart_write(SERIAL_THR, (u8)c);
+}
+
+void serial_puts(char *str)
+{
+	while (*str) {
+		serial_putc(*str);
+		str++;
+	}
 }
--- src/trap.c
+++ src/trap.c
@@ -1,53 +1,85 @@
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <kernel/printf.h>
+#include <arch/csr.h>
+#include <arch/plic.h>
+#include <arch/timer.h>
+#include <kernel/serial.h>
 
-/* defined in src/trap_entry.S */
+/* definido em src/trap_entry.S */
 extern void trap_entry();
-
-void handle_irq()
-{
-	/* not implemented */
-	BUG();
-}
-
-void handle_exception()
-{
-	/* not implemented */
-	BUG();
-}
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
-}
-
-void handle_trap()
-{
-	/* not implemented */
-	BUG();
+	/* aponta o stvec pro nosso handler de trap em assembly */
+	csr_write(CSR_STVEC, (u64)trap_entry);
 }
 
 void hart_irq_enable()
 {
-	/* not implemented */
-	BUG();
-}
-
-u64 hart_irq_save()
-{
-	/* not implemented */
-	BUG();
-}
-
-void hart_irq_restore(u64 flags)
-{
-	/* not implemented */
-	BUG();
+	/* liga o bit SIE em sstatus, habilitando interrupções nesse hart */
+	csr_set(CSR_SSTATUS, CSR_SSTATUS_SIE);
 }
 
 void hart_irq_disable()
 {
-	/* not implemented */
-	BUG();
+	/* desliga o bit SIE, desabilitando interrupções */
+	csr_clear(CSR_SSTATUS, CSR_SSTATUS_SIE);
+}
+
+u64 hart_irq_save()
+{
+	/* lê o estado atual de SIE e já desliga, tudo em uma instrução atômica */
+	return csr_read_clear(CSR_SSTATUS, CSR_SSTATUS_SIE) & CSR_SSTATUS_SIE;
+}
+
+void hart_irq_restore(u64 flags)
+{
+	/* volta o estado de SIE que foi salvo por hart_irq_save() */
+	csr_set(CSR_SSTATUS, flags & CSR_SSTATUS_SIE);
+}
+
+void handle_irq()
+{
+	u64 scause = csr_read(CSR_SCAUSE);
+
+	if (scause == TRAP_TIMER_IRQ) {
+		timer_irq();
+	} else if (scause == TRAP_EXTERNAL_IRQ) {
+		/* interrupção externa (ex: serial) precisa do handshake com o PLIC:
+		 * primeiro reivindicamos (claim) qual IRQ aconteceu */
+		u32 irq = plic_hart_claim_irq(0);
+		if (irq == IRQ_SERIAL) {
+			serial_irq();
+		}
+		/* depois avisamos o PLIC que terminamos de tratar */
+		if (irq) {
+			plic_hart_complete_irq(0, irq);
+		}
+	}
+}
+
+void handle_exception()
+{
+	u64 scause = csr_read(CSR_SCAUSE);
+	u64 stval = csr_read(CSR_STVAL);
+	u64 sepc = csr_read(CSR_SEPC);
+
+	/* não tratamos exceções específicas neste lab, então só reportamos
+	 * o que aconteceu e paramos o kernel */
+	panic("unhandled exception: scause=%lx stval=%lx sepc=%lx\n",
+	      scause, stval, sepc);
+}
+
+void handle_trap()
+{
+	u64 scause = csr_read(CSR_SCAUSE);
+
+	/* o bit mais significativo de scause diz se é interrupção (1) ou
+	 * exceção síncrona (0) */
+	if (scause & TRAP_IRQ_BIT) {
+		handle_irq();
+	} else {
+		handle_exception();
+	}
 }
